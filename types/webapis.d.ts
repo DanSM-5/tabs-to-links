65b480284d1fc338a3dceae086c969ba5d674696
daniel <eduardo.daniel.s.m@gmail.com>
2024-08-24 19:17:20 -0400

    fix: correct type definition for withResolvers

--- types/webapis.d.ts
+++ types/webapis.d.ts
@@ -1,11 +1,12 @@
 interface PromiseConstructor {
   withResolvers: <T>() => {
     promise: Promise<T>;
-    reject: Promise.reject;
-    resolve: Promise.resolve<T>;
+    reject:  typeof Promise.reject<T>;
+    resolve: typeof Promise.resolve<T>;
   }
 }
 
 interface ErrorConstructor {
   new (message?: string, options?: { cause?: unknown })
 }
+
